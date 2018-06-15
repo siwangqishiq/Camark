a144ccc8ee0c8fa586a8f44cc28469d5962c9ad4
panyi <yuyi.py@alibaba-inc.com>
2018-06-15 15:27:34 +0800

    PyMario

---
+++ PyMario/PyMario/Camera.cpp
@@ -0,0 +1,9 @@
+//
+//  Camera.cpp
+//  PyMario
+//
+//  Created by panyi  on 2018/6/13.
+//  Copyright © 2018年 xinlan.com. All rights reserved.
+//
+
+#include "Camera.hpp"
---
+++ PyMario/PyMario/Config.h
@@ -0,0 +1,18 @@
+//
+//  Config.h
+//  PyMario
+//
+//  Created by panyi  on 2018/6/13.
+//  Copyright © 2018年 xinlan.com. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+#define APPTITLE         "Super Mario"
+
+const int SCREEN_WIDTH     = 800;
+const int SCREEN_HEIGHT    = 600;
+
+
+#endif /* Config_h */
